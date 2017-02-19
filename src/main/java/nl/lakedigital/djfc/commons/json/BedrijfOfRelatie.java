963fe22d74f64a067a37880d17ebe1184e451e51
Patrick Heidotting <patrick@lakedigital.nl>
2017-02-19 18:36:34 +0100

    SYMPLEX-72 Nieuwe lay-out voor het zoekscherm

--- src/main/java/nl/lakedigital/djfc/commons/json/BedrijfOfRelatie.java
+++ src/main/java/nl/lakedigital/djfc/commons/json/BedrijfOfRelatie.java
@@ -2,4 +2,12 @@ package nl.lakedigital.djfc.commons.json;
 
 public abstract class BedrijfOfRelatie {
     private JsonAdres adres;
+
+    public JsonAdres getAdres() {
+        return adres;
+    }
+
+    public void setAdres(JsonAdres adres) {
+        this.adres = adres;
+    }
 }
