bd86aeade0b59940a829048f180f5916140af485
Patrick Heidotting <patrick@lakedigital.nl>
2017-09-16 20:17:57 +0200

    LogglyAppender gemaakt

---
+++ src/main/java/nl/lakedigital/djfc/appender/LogglyAppender.java
@@ -0,0 +1,66 @@
+package nl.lakedigital.djfc.appender;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.spi.LoggingEvent;
+
+public class LogglyAppender extends AppenderSkeleton {
+    private String tag = "";
+    private String token = "";
+
+    public LogglyAppender() {
+        super();
+    }
+
+    public LogglyAppender(boolean isActive) {
+        super(isActive);
+    }
+
+
+    @Override
+    protected void append(LoggingEvent event) {
+
+        /**
+         * We always only produce to the current file. So there's no need for locking
+         */
+
+        assert this.layout != null : "Cannot log, there is no layout configured.";
+
+        String output = this.layout.format(event);
+
+        try {
+            HttpResponse<String> response = Unirest.post("http://logs-01.loggly.com/inputs/" + token + "/tag/" + tag + "," + event.getLevel()).header("accept", "application/json").body(output).asString();
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public boolean requiresLayout() {
+        return false;
+    }
+}
